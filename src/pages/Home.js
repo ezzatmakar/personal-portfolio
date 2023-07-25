46eb589504d5980cfc70e33dd17dfdc09159507d
Ezzat Malak <39204454+ezzatmakar@users.noreply.github.com>
2023-07-25 13:39:17 +0300

    Fix home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,27 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from "@material-ui/icons/Email";
 import GithubIcon from "@material-ui/icons/GitHub";
 import { Link } from "react-router-dom";
+import Typed from "react-typed";
 import "../styles/Home.css";
 
 function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2> Hi, My Name is Ezzat Makar</h2>
+        <h1>
+          Hi, My Name is Ezzat Makar
+        </h1>
         <div className="prompt">
-          <p>A software developer with a passion for learning and creating.</p>
+          <Typed
+            strings={[
+              "I'm software developer with a passion for learning and creating",
+              "I'm currently developing various open-source projects"
+            ]}
+            typeSpeed={40}
+            backSpeed={50}
+            loop
+          />
+          <br></br>
           <Link to='mailto:ezzatmakar93@gmail.com'><EmailIcon /></Link>
           <Link to='https://github.com/ezzatmakar'><GithubIcon /></Link>
           <Link to='https://www.linkedin.com/in/ezzat-makar/'><LinkedInIcon /></Link>
@@ -21,6 +33,20 @@ function Home() {
         <h1> Skills</h1>
         <ol className="list">
           <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              NodeJS, NestJS, ExpressJs, Laravel, Zend Framework.
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, PHP, TypeScript</span>
+          </li>
+          <li className="item">
+            <h2>DataBases</h2>
+            <span>MySQL, MongoDB, PostgreSQL, Redis</span>
+          </li>
+          <li className="item">
             <h2> Front-End</h2>
             <span>
               ReactJS, VueJS, HTML, CSS, jQuery,
@@ -28,14 +54,8 @@ function Home() {
             </span>
           </li>
           <li className="item">
-            <h2>Back-End</h2>
-            <span>
-              NodeJS, NestJS, ExpressJs, Laravel, Zend Framework MySQL, MongoDB, PostgreSQL, Redis.
-            </span>
-          </li>
-          <li className="item">
-            <h2>Languages</h2>
-            <span>JavaScript, PHP, TypeScript</span>
+            <h2>Tools</h2>
+            <span>Docker, Git, Terminal</span>
           </li>
         </ol>
       </div>
