dfcb69420269c0d1b1d78d945ebf2c74194b06b9
Ezzat Malak <39204454+ezzatmakar@users.noreply.github.com>
2023-07-25 15:00:13 +0300

    SEO optimization

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import Home from './pages/Home';
 import Experience from './pages/Experience';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { Helmet } from 'react-helmet';
+
 
 function App() {
   return (
     <div className='App'>
+      <Helmet>
+        <title>Ezzat Makar | Software Engineer Portfolio</title>
+        <meta name="description" content="Welcome to Ezzat Makar's personal portfolio website! As a passionate software engineer, I showcase my latest projects, skills, and experiences in the world of software development." />
+      </Helmet>
       <Router>
         <Navbar />
         <Routes >
